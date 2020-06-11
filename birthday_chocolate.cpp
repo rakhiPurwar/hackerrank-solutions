e43ef82646c08a819ec58508c5855fd548543996
Rakhi Purwar <42908255+rakhiPurwar@users.noreply.github.com>
2020-06-11 13:16:33 +0530

    Create birthday_chocolate.cpp

---
+++ birthday_chocolate.cpp
@@ -0,0 +1,23 @@
+int birthday(vector<int> s, int d, int m) {
+    int sum=0,i,end,count=0;
+    for(i=0;i<m;i++)
+    {
+        sm += s[i];
+    }
+    if(sum== d)
+        count+=1;
+    cout<<sum;
+    for(end=m;end<s.size();end++)
+    {
+        sum = sum + s[end] - s[end-m];
+        cout<<sum;
+        if(sum == d)
+          count+=1;
+        else
+           count+=0;
+        
+    }
+    return count;
+
+
+}
