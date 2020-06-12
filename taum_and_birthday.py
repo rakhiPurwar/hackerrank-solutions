ec11750107c70506923c60872a226981c4fe1def
Rakhi Purwar <42908255+rakhiPurwar@users.noreply.github.com>
2020-06-12 21:27:08 +0530

    Create taum_and_birthday.py

---
+++ taum_and_birthday.py
@@ -0,0 +1,13 @@
+def taumBday(b, w, bc, wc, z):
+
+    # Write your code here
+    if (bc>wc+z)  and (bc > wc):
+        return (b+w)*wc+(z*b)
+     
+    
+    elif wc>bc+z and wc>bc:
+    
+        return (b+w)*bc+(z*w)
+    else:
+        return b*bc+w*wc
+    
